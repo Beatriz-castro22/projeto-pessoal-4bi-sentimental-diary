28e16eb5e5a7300f7c1cb9997749d56e2d21d7cf
Bia★ <anabiafernandes.222@gmail.com>
2024-11-18 12:01:03 -0300

    up

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const sentiments = []
 const option = ''
 const nome = prompt('Oiii bem vindo ao sentimental diary ,por gentileza digite seu nome <3' )
-alert(`Ola ${nome} qual sentimento voce esta se sentido hoje
+alert(`Olá ${nome} qual sentimento você está sentido hoje? ⁠☆ﾟ⁠.⁠*⁠･⁠｡ﾟ
     1-Feliz
     2-Triste
-    3-Raiva
+    3-Raiva 
     4-Ansioso
     5-Solitario
     6-sair` )
